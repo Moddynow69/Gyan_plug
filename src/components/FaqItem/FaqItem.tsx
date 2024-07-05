fa260ebc260c050e8a50efaea866595fb9f75466
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2024-07-06 01:53:27 +0530

    Added faq page

--- src/components/FaqItem/FaqItem.tsx
+++ src/components/FaqItem/FaqItem.tsx
@@ -12,7 +12,7 @@ export default function FaqItem({ heading, paragraph }: Props) {
 
   return (
     <div
-      className={`flex flex-col w-[100%] gap-[1.5rem] justify-between duration-500 ${
+      className={`flex flex-col w-[100%] gap-[1.5rem] justify-between duration-700 ${
         isVisible ? "h-[9.2rem]" : ""
       }`}
     >
@@ -27,23 +27,23 @@ export default function FaqItem({ heading, paragraph }: Props) {
             alt="->"
             id="arrow"
             src="images/Next-button.svg"
-            className={`transform transition-transform duration-500 ease ${
+            className={`transform transition-transform duration-700 ease ${
               isVisible ? "rotate-90" : "rotate-0"
             } flex-shrink-0`}
           />
-          <div className="flex items-center gap-[2.25rem] text-[#041020] text-[1.5rem] font-[700] leading-[140%]">
+          <div className="flex items-center gap-[2.25rem] text-[#041020] text-[1.5rem] font-[600] leading-[140%]">
             {heading}
           </div>
         </div>
         <p
           className={`absolute top-[3.5rem] transition-all ease flex justify-start flex-1 font-medium text-white text-1xl pt-[1rem] pb-[1rem] pl-[1.5rem] pr-[1.5rem] leading-[1.75rem] items-start bg-[rgba(20,164,225,1)] rounded-lg overflow-hidden w-[100%] ${
-            isVisible ? "opacity-100 duration-500" : "duration-500 opacity-0 "
+            isVisible ? "opacity-100 duration-700" : "duration-700 opacity-0 "
           }`}
         >
           {paragraph}
         </p>
       </div>
-      <hr className="border-[rgba(20,164,225,0.20)] border-[1px] w-[100%] transition-all ease duration-500" />
+      <hr className="border-[rgba(20,164,225,0.20)] border-[1px] w-[100%] transition-all ease duration-700" />
     </div>
   );
 }
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,8 +2,8 @@ import { Props } from "next/script";
 
 export default function Heading({content}: Props) {
   return (
-    <div className="text-center font-folito text-[54px] font-black leading-[60px] bg-clip-text text-transparent bg-gradient-to-r from-[#041020] to-[#0174BE]">
-        {content}
+    <div className="text-center font-folito text-[54px] font-black leading-[60px]">
+        <span className="bg-text-gradient bg-clip-text text-transparent">{content}</span>
     </div>
   );
 }
