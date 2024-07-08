565273fb08eec77972c13d732f26c12655d71196
Tushar <tusharkaler450@gmail.com>
2024-07-08 22:20:49 +0530

    white space

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Heading({content,from,to}: Props) {
   return (
     <div className="text-center font-folito text-[54px] font-black leading-[60px]">
-        <span className=" bg-clip-text text-transparent"
+        <span className=" bg-clip-text text-transparent whitespace-nowrap"
         style={{
           backgroundImage: `linear-gradient(90deg, ${from} 0%, ${to} 100%)`
         }}>{content}</span>
