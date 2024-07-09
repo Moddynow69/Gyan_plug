b15a8188138ecf63b05feeaf9e27a3fbb5dc2930
Tushar <tusharkaler450@gmail.com>
2024-07-10 03:39:02 +0530

    1 section left

--- src/app/_sections/HowItWorks/How.tsx
+++ src/app/_sections/HowItWorks/How.tsx
@@ -7,7 +7,7 @@ export default function How() {
     <div id="how-it-works" className="h-auto lg:h-[1074px] w-screen flex flex-col relative overflow-hidden pt-20 gap-[72px]">
       <Heading content="How It Works" from="#041020" to="#0174BE" />
       <div className="h-auto lg:h-[767px] w-full  2xl:px-[120px] xl:px-20 px-10 flex items-start relative overflow-y-scroll">
-        <div className="h-auto w-full  flex flex-col gap-4 lg:gap-[150px] xl:pb-[278px] lg:pb-[354px]">
+        <div className="h-auto w-full  flex flex-col gap-4 lg:gap-[150px] pb-[72px] lg:pb-[354px] xl:pb-[278px]">
           {HowCardData.map((card, index) => (
             <HowCard key={index} {...card} idx={index + 1} />
           ))}
--- src/app/_sections/Mentors/Mentors.tsx
+++ src/app/_sections/Mentors/Mentors.tsx
@@ -4,13 +4,17 @@ import Heading from "@/components/Heading/Heading";
 
 export default function Mentors() {
   return (
-    <div id="mentors" className="h-[962px] w-full flex items-end lg:pb-[38px] relative">
-      <div className="w-auto h-auto absolute top-[54px] left-1/2 translate-x-[-50%]">
-        <Heading content="Meet Your Mentors" from="#041020" to="#0174BE"/>
+    <div
+      id="mentors"
+      className="h-auto md:h-[962px] w-full flex flex-col gap-6 md:gap-0 items-center md:items-end lg:pb-[38px] relative"
+    >
+      <div className="w-auto h-auto md:absolute md:top-[54px] md:left-1/2 md:translate-x-[-50%]">
+        <Heading content="Meet Your Mentors" from="#041020" to="#0174BE" />
       </div>
-      <MentorsCarousel/>
-      <div className="w-auto h-auto absolute bottom-[98.29px] left-1/2 translate-x-[-50%]">
-      <ButtonComponent content="View All" type="secondary"/>
+      <MentorsCarousel />
+      <div className="w-auto h-auto absolute bottom-[7px] md:bottom-[98.29px] left-1/2 translate-x-[-50%]">
+        <ButtonComponent content="View All" type="secondary" className="hidden md:flex"/>
+        <ButtonComponent content="View All" type="secondary" arrow={false} className="w-[144px] h-[43px] md:hidden"/>
       </div>
     </div>
   );
