8551e1b26b4dcadd44dbc502342091944bbe840d
Tushar <tusharkaler450@gmail.com>
2024-07-11 00:19:51 +0530

    almost final

--- src/components/Carousels/LogoCarousel/LogoCarouselItem.tsx
+++ src/components/Carousels/LogoCarousel/LogoCarouselItem.tsx
@@ -8,7 +8,7 @@ export default function LogoCarouselItem({
 }) {
   return (
     <div className="md:h-[109px] h-[67.56px] md:w-[109px] w-[67.56px] flex justify-center items-center rounded-xl border-[0.43px] border-[#14A4E166]">
-      <Image src={src} alt={alt} height={82} width={82} />
+      <Image src={src} alt={alt} height={82} width={82} className="rounded-xl"/>
     </div>
   );
 }
