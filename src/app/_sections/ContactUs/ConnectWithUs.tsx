681fb7a60f40c2421c13ae6d88d338f98c551b71
Tushar <tusharkaler450@gmail.com>
2024-07-23 22:20:44 +0530

    final shut

--- src/app/_sections/ContactUs/ConnectWithUs.tsx
+++ src/app/_sections/ContactUs/ConnectWithUs.tsx
@@ -11,28 +11,28 @@ export default function ConnectWithUs() {
       >
         <div className="hidden lg:block w-[320px] lg:w-auto absolute lg:static">
           <Image
-            src="/images/person1.png"
+            src="/images/Person_Images/person1.png"
             width={107}
             height={107}
             alt="*"
             className="relative w-[49px] lg:w-[107px] -left-[12.8px] top-6 lg:-top-2 lg:left-0 xl:-top-6 xl:left-20 2xl:w-auto 2xl:-top-7 2xl:left-48"
           />
           <Image
-            src="/images/person2.png"
+            src="/images/Person_Images/person2.png"
             width={111}
             height={111}
             alt="*"
             className="hidden lg:block relative lg:left-[64px] xl:-top-2 xl:left-[208px] 2xl:-top-0 2xl:left-[352px]"
           />
           <Image
-            src="/images/person3.png"
+            src="/images/Person_Images/person3.png"
             width={165}
             height={165}
             alt="*"
             className="w-[38px] lg:w-[165px] left-[54.4px] -top-[82.88] relative lg:-top-4 lg:-left-6 xl:-top-4 xl:left-[16px] 2xl:-top-7 2xl:left-[96px]"
           />
           <Image
-            src="/images/person4.png"
+            src="/images/Person_Images/person4.png"
             width={87}
             height={87}
             alt="*"
@@ -57,28 +57,28 @@ export default function ConnectWithUs() {
         </div>
         <div className="hidden lg:block w-[320px]  lg:w-auto absolute lg:static overflow-x-clip sm:overflow-visible">
           <Image
-            src="/images/person5.png"
+            src="/images/Person_Images/person5.png"
             width={86}
             height={86}
             alt="*"
             className="hidden lg:block relative lg:-top-4 lg:-right-12 xl:-top-4 right-0 2xl:right-0 2xl:-top-4"
           />
           <Image
-            src="/images/person6.png"
+            src="/images/Person_Images/person6.png"
             width={139}
             height={139}
             alt="*"
             className="relative w-[40px] -right-[232px] top-7 lg:w-[139px] lg:-top-2 lg:right-8 xl:-top-4 xl:right-[160px] 2xl:right-[272px] 2xl:-top-4"
           />
           <Image
-            src="/images/person7.png"
+            src="/images/Person_Images/person7.png"
             width={113}
             height={113}
             alt="*"
             className="relative w-[39px] -right-48 -top-[72px] lg:w-[113px] lg:-top-4 lg:-right-[32px] 2xl:right-[128px] 2xl:-top-4"
           />
           <Image
-            src="/images/person8.png"
+            src="/images/Person_Images/person8.png"
             width={102}
             height={102}
             alt="*"
