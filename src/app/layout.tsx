5a52f2e5c3d03d8bab796ff12e9ff9dd00799479
Tushar <tusharkaler450@gmail.com>
2024-07-29 09:33:56 +0530

    again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { folito, inter } from "./fonts";
-import Image from "next/image";
 
-import { ImageResponse } from 'next/og'
- 
-export const runtime = 'edge'
- 
-export async function GET() {
-  return new ImageResponse(
-    (
-      <Image
-        height={600}
-        width={1200}
-        src="https://example.com/images/THBHNL.jpg"
-        alt="A beautiful image"
-      />
-    ),
-    {
-      width: 1200,
-      height: 600,
-    }
-  )
-}
 export const metadata: Metadata = {
   title: "Gyan Plug",
   description: "Education Counselors",
