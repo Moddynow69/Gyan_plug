782d25f894c9bd1f8bbaaa00ddf63a2a0b32d365
Tushar <tusharkaler450@gmail.com>
2025-02-12 16:45:14 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { folito, inter } from "./fonts";
+import Head from "next/head";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "GyanPlug",
@@ -11,7 +13,7 @@ export const metadata: Metadata = {
     type: "website",
     locale: "en_US",
     siteName: "Gyan Plug",
-    images: [ { url: "https://ibb.co/17ryW35" } ],
+    images: [{ url: "https://ibb.co/17ryW35" }],
   },
 };
 
@@ -22,6 +24,55 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1391313745164909');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1391313745164909&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=YOUR-GA-ID"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'YOUR-GA-ID');
+          `}
+        </Script>
+
+        {/* Mailchimp */}
+        <Script id="mcjs" strategy="afterInteractive">
+          {`
+            !function(c,h,i,m,p){
+              m=c.createElement(h),
+              p=c.getElementsByTagName(h)[0],
+              m.async=1,
+              m.src=i,
+              p.parentNode.insertBefore(m,p)
+            }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/515e9fdcc4823ccc133f0cfca/d69ab3a568bfa6c2a92142e34.js");
+          `}
+        </Script>
       <body className={`${inter.className} ${folito.className}`}>
         {children}
       </body>
--- src/constants/Home/Home.tsx
+++ src/constants/Home/Home.tsx
@@ -2,7 +2,7 @@ const home = {
   heading1: "Shape Your ",
   spanHeading: "Future With",
   heading2: "India's Top Admission Counsellors.",
-  subHeading: "Discover The College & Course That Aligns With Your Goals!",
+  subHeading: "Discover The College and Course That Aligns With Your Goals!",
   button: "Book A Free Session",
   buttonLink: "http://app.gyanplug.com/",
   image: "/images/Map.gif",
